e4ac7042ee3526b62ce43af427011781ab1e2f83
Jochem Pouwels <pouwelsjochem@gmail.com>
2020-09-07 11:56:07 +0200

    Removed 'scale' attribute from config.lua

--- Corona/config.lua
+++ Corona/config.lua
@@ -3,10 +3,6 @@ application =
 	content =
 	{
 		width = 320,
-		height = 480,
-		scale = "zoomEven", 
-		-- scale = "none", 
-		-- scale = "letterbox",
-		-- scale = "zoomStretch", 
+		height = 480
 	},
 } 
